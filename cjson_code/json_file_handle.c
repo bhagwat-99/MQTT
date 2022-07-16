00cce7209ee974c6b15474ddc6f371f3ec3e9ea2
bhagwat-99 <shindebhagavat030@gmail.com>
2022-07-16 14:01:35 +0530

    provision code update

--- cjson_code/json_file_handle.c
+++ cjson_code/json_file_handle.c
@@ -5,8 +5,9 @@
 #include <fcntl.h>
 #include "cjson/cJSON.h"
 
+#define CONF_FILE_PATH "/etc/gateway/gateway.conf"
 
-char * data_buf;
+void fail(void) {printf("error allocating memory \n"); exit(EXIT_FAILURE);}
 
 
 typedef struct DEVICE
@@ -14,6 +15,7 @@ typedef struct DEVICE
 
 	char *certOwnership_token;
 	char device_ID[10];
+	char 	client_ID[20];
 
 }DEVICE;
 
@@ -26,10 +28,9 @@ typedef struct CLOUD
 
 }CLOUD;
 
-DEVICE gateway_device;
-CLOUD gateway_cloud;
 
-char * read_config_file(char * filename,char * mode)
+
+int read_config_file(char * filename,char * mode,char * data)
 {
 	FILE * fd;
 	fd = fopen(filename,mode);
@@ -38,22 +39,22 @@ char * read_config_file(char * filename,char * mode)
 		printf("error opening file : /etc/gateway/gateway.conf");
 		exit(1);
 	}
-	fread(data_buf,4000,1,fd);
+	fread(data,4000,1,fd);
 	fclose(fd);
-	return data_buf;
+	return 0;
 
 }
 
-int parse_json_file(const char * jsonfile)
+int parse_json_file(const char * jsonfile, DEVICE *device, CLOUD *cloud)
 {
-	char * data;
-	cJSON * cloud = NULL;
+	cJSON * jcloud = NULL;
 	cJSON * aws_url = NULL;
+	cJSON * client_ID = NULL;
 	cJSON * pub_topic_telemetry = NULL;
 	cJSON * pub_topic_status = NULL;
 	cJSON * sub_topic_request = NULL;
 
-	cJSON * device = NULL;
+	cJSON * jdevice = NULL;
 	cJSON * serial_ID = NULL;
 	
 
@@ -71,26 +72,26 @@ int parse_json_file(const char * jsonfile)
 
 	//storing parsed data into device gateway and cloud structure
 
-	device 	= cJSON_GetObjectItemCaseSensitive(gateway,"device");
-	serial_ID = cJSON_GetObjectItemCaseSensitive(device,"SERIAL_ID");
-	strcpy(gateway_device.device_ID,cJSON_Print(serial_ID));
+	jdevice 	= cJSON_GetObjectItemCaseSensitive(gateway,"device");
+	serial_ID = cJSON_GetObjectItemCaseSensitive(jdevice,"SERIAL_ID");
+	strcpy(device->device_ID,cJSON_Print(serial_ID));
+
+	client_ID = cJSON_GetObjectItemCaseSensitive(jdevice,"NAME");
+	strcpy(device->client_ID,cJSON_Print(client_ID));
 
 
-	cloud 	= cJSON_GetObjectItemCaseSensitive(gateway,"cloud");
-	aws_url = cJSON_GetObjectItemCaseSensitive(cloud,"HOST");
-	strcpy(gateway_cloud.aws_url,cJSON_Print(aws_url));
+	jcloud 	= cJSON_GetObjectItemCaseSensitive(gateway,"cloud");
+	aws_url = cJSON_GetObjectItemCaseSensitive(jcloud,"HOST");
+	strcpy(cloud->aws_url,cJSON_Print(aws_url));
 
-	aws_url = cJSON_GetObjectItemCaseSensitive(cloud,"HOST");
-	strcpy(gateway_cloud.aws_url,cJSON_Print(aws_url));
+	pub_topic_telemetry = cJSON_GetObjectItemCaseSensitive(jcloud,"TELEMETRY_PUBLISH_TOPIC");
+	strcpy(cloud->pub_topic_telemetry,cJSON_Print(pub_topic_telemetry));
 
-	pub_topic_telemetry = cJSON_GetObjectItemCaseSensitive(cloud,"TELEMETRY_PUBLISH_TOPIC");
-	strcpy(gateway_cloud.pub_topic_telemetry,cJSON_Print(pub_topic_telemetry));
+	pub_topic_status = cJSON_GetObjectItemCaseSensitive(jcloud,"STATUS_PUBLISH_TOPIC");
+	strcpy(cloud->pub_topic_status,cJSON_Print(pub_topic_status));
 
-	pub_topic_status = cJSON_GetObjectItemCaseSensitive(cloud,"STATUS_PUBLISH_TOPIC");
-	strcpy(gateway_cloud.pub_topic_status,cJSON_Print(pub_topic_status));
-
-	sub_topic_request = cJSON_GetObjectItemCaseSensitive(cloud,"JOB_TOPIC");
-	strcpy(gateway_cloud.sub_topic_request,cJSON_Print(sub_topic_request));
+	sub_topic_request = cJSON_GetObjectItemCaseSensitive(jcloud,"JOB_TOPIC");
+	strcpy(cloud->sub_topic_request,cJSON_Print(sub_topic_request));
 
 
 
@@ -103,46 +104,48 @@ int parse_json_file(const char * jsonfile)
 }
 
 
-char * create_register_thing_payload(struct DEVICE device)
+int create_register_thing_payload(struct DEVICE *device, char * jdata)
 {
 
 	/*Creating a json object*/
 	cJSON * jobj = cJSON_CreateObject();
 	
 	/*Creating a json string*/
-	cJSON * jcertownertoken = cJSON_CreateString(device.certOwnership_token);
+	cJSON * jcertownertoken = cJSON_CreateString(device->certOwnership_token);
 	cJSON_AddItemToObject(jobj,"certificateOwnershipToken", jcertownertoken);
 
 	cJSON * jparameters = cJSON_CreateObject();
 	cJSON_AddItemToObject(jobj, "parameters", jparameters);
 
-	cJSON *jdevice_ID = cJSON_CreateString(device.device_ID);
+	cJSON *jdevice_ID = cJSON_CreateString(device->device_ID);
 	cJSON_AddItemToObject(jparameters,"SerialNumber", jdevice_ID);
 
-	// printf ("%s\n",json_object_to_json_string(jobj));
-	// ppayload[0]='\0';
-	//strcpy(ppayload, json_object_to_json_string(jobj));
-
-	strcpy(data_buf,cJSON_Print(jobj));
+	strcpy(jdata,cJSON_Print(jobj));
 
 	cJSON_Delete(jobj);
 	
 
-	return data_buf;
+	return 0;
 }
 
 int main()
 {
-	data_buf = malloc(4000);
-	if(data_buf == NULL)
-	{
-		printf("error allocating memory for data_buf");
-	}
+	DEVICE gateway_device;
+	CLOUD gateway_cloud;
 
-	read_config_file("/etc/gateway/gateway.conf","r");
 
-	parse_json_file(data_buf);
+	char * data_buf = malloc(4000 * sizeof(char));
+	if (data_buf == NULL) fail();
+
+	read_config_file(CONF_FILE_PATH,"r",data_buf);
+
+	parse_json_file(data_buf,&gateway_device,&gateway_cloud);
+
+	create_register_thing_payload(gateway_device,data_buf);
+
+	printf("%s",data_buf);
+	//printf("%s\n",gateway_device.device_ID);
 	
-
+	free(data_buf);
 	return 0;
 }
